193c06abeb418958891f27c60fdd9f5e75a1b81d
Ali Nejat Yucad <aliyucad@horizonistanbul.com>
2018-11-20 17:42:49 +0300

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Ali Nejat Yucad on 20.11.2018.
+//  Copyright © 2018 Ali Nejat Yucad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
